786d662d2d04bf9cdc7b5afd7a5e9237cfc205eb
Manuel <emmanuelokinyi799@gmail.com>
2024-04-12 18:29:16 +0300

    colors

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function displayPosts(posts){
      for(post of posts){
     cardsContainer.innerHTML += `
         <div>
-            <div href="#" class="block mx-auto max-w-sm p-6 bg-white h-screenborder border-gray-200 rounded-lg shadow  w-fullhover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div href="#" class="block mx-auto max-w-sm p-6 bg-gray-900 h-screen border border-gray-200 rounded-lg shadow  w-fullhover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                   <img class="w-full" src="${post.img}" alt=Loading">
                   <div class="flex justify-between">
-                <i onclick="deletePost(${post.id})" class=" fa fa-trash-o text-3xl" aria-hidden="true"></i>
-                <i class="fa fa-pencil-square-o text-3xl  aria-hidden="true"></i>
+                <i onclick="deletePost(${post.id})" class=" fa fa-trash-o text-white text-3xl" aria-hidden="true"></i>
+                <i class="fa fa-pencil-square-o text-3xl  text-white aria-hidden="true"></i>
                   </div>
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${post.title}</h5>
-                    <p class="font-normal text-gray-700 dark:text-gray-400">${post.description}</p>
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-200 dark:text-white">${post.title}</h5>
+                    <p class="font-normal text-gray-400 dark:text-gray-400">${post.description}</p>
               </div>
         </div>  
     `
