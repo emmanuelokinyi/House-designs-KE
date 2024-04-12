07e4fab84ef36c4a5ebcdce86d971fa4fed2fdfe
Manuel <emmanuelokinyi799@gmail.com>
2024-04-12 13:56:41 +0300

    commit 2

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-fetch('http://localhost:3000')
-  .then((data) => data.json())
-  .then((posts) =>{
+fetch("http://localhost:3000/posts")
+.then((data) => data.json()  )
+.then((posts) =>{
   console.log(posts);
-  })
+   let cardsContainer= document.getElementById("cardsContainer")
+   cardsContainer.innerHTML
+     for(post of posts){
+    cardsContainer.innerHTML += `
+        <div>
+            <div href="#" class="block mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow w-full hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                  <img class="w-full" src="${post.img}" alt=Loading">
+              
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${post.title}</h5>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">${post.description}</p>
+              </div>
+        </div>  
+    `
+  }
+})
+
   
 
   
