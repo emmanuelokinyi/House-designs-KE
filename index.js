7cef2233deb14e69c57720ed0055f649302bd41b
Manuel <emmanuelokinyi799@gmail.com>
2024-04-12 14:39:03 +0300

    commit 2

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ fetch("http://localhost:3000/posts")
      for(post of posts){
     cardsContainer.innerHTML += `
         <div>
-            <div href="#" class="block mx-auto max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow w-full hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <div href="#" class="block mx-auto max-w-sm p-6 bg-white h-screen border border-gray-200 rounded-lg shadow  w-fullhover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                   <img class="w-full" src="${post.img}" alt=Loading">
-              
+                  <div class="flex justify-between">
+                <i class=" fa fa-trash-o text-3xl" aria-hidden="true"></i>
+                <i class="fa fa-pencil-square-o text-3xl  aria-hidden="true"></i>
+                  </div>
                   <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${post.title}</h5>
                     <p class="font-normal text-gray-700 dark:text-gray-400">${post.description}</p>
               </div>
